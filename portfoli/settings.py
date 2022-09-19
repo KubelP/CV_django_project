04cfd1fc6a94b0f4cb5d72e091215d53338ecbf8
Quantum <jakubpiergies@gmail.com>
2022-09-19 18:46:22 +0200

    removed from settings.py SECRET_KEY

--- portfoli/settings.py
+++ portfoli/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret_key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zsnqtma+t5ta0u17ps^(9@rgm8l97ex6onslhm04ni=ho*1k$k'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
